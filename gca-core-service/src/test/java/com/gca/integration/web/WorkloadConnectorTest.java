253036350668c9b2a2c8f2ffcf4185c382562cf7
ArturGoz <arturrm4@gmail.com>
2025-09-07 21:37:04 +0300

    GCA - 158 Implement microservices authorization - enrich workload connector with authentication context

--- gca-core-service/src/test/java/com/gca/integration/web/WorkloadConnectorTest.java
+++ gca-core-service/src/test/java/com/gca/integration/web/WorkloadConnectorTest.java
@@ -7,14 +7,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import static com.gca.controller.ApiConstant.TRAINER_WORKLOAD_URL;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -33,19 +38,30 @@ class WorkloadConnectorTest {
     @BeforeEach
     void setUp() {
         request = new TrainerWorkloadDTO();
+        request.setTrainerUsername("john_doe");
     }
 
     @Test
     void processTrainerWorkloadRequest_success() {
+        MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(mockRequest));
         ResponseEntity<Void> expected = ResponseEntity.ok().build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<TrainerWorkloadDTO> httpEntity = new HttpEntity<>(request, headers);
 
-        when(restTemplate.postForEntity(anyString(), eq(request), eq(Void.class)))
+        when(restTemplate.postForEntity(eq(TRAINER_WORKLOAD_URL), eq(httpEntity), eq(Void.class)))
                 .thenReturn(expected);
 
         ResponseEntity<Void> actual = sut.processTrainerWorkloadRequest(request);
 
         assertThat(actual).isEqualTo(expected);
-        verify(restTemplate).postForEntity(anyString(), eq(request), eq(Void.class));
+        verify(restTemplate).postForEntity(
+                eq(TRAINER_WORKLOAD_URL),
+                eq(httpEntity),
+                eq(Void.class)
+        );
+        RequestContextHolder.resetRequestAttributes();
     }
 
     @Test
@@ -55,7 +71,7 @@ class WorkloadConnectorTest {
         ResponseEntity<Void> actual = ReflectionTestUtils.invokeMethod(sut,
                 "fallback", request, ex);
 
-        assertNotNull(actual);
+        assertThat(actual).isNotNull();
         assertThat(actual.getStatusCode()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
